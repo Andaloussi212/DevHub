02a149515912373eb6adce06786852129e325c6e
Andaloussi212 <zayd.benz.pro@proton.me>
2026-05-11 22:08:52 +0200

    Ajout de filtre pour la parties commandes

--- script.js
+++ script.js
@@ -14,3 +14,30 @@ btnRetour.addEventListener('click', function () {
     behavior: 'smooth',
   });
 });
+
+const btnFiltre = document.querySelectorAll('.filtre-btn');
+const commandCards = document.querySelectorAll('#commandes article');
+
+btnFiltre.forEach((button) => {
+  button.addEventListener('click', function () {
+    const filtreSelectionnee = button.dataset.filter;
+
+    btnFiltre.forEach((btn) => {
+      btn.classList.remove('active');
+    });
+
+    button.classList.add('active');
+    commandCards.forEach((card) => {
+      const CategorieCarte = card.dataset.category;
+
+      if (
+        filtreSelectionnee === 'all' ||
+        filtreSelectionnee === CategorieCarte
+      ) {
+        card.style.display = 'block';
+      } else {
+        card.style.display = 'none';
+      }
+    });
+  });
+});
